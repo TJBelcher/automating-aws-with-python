f23593a733956d78b3e16b688bdc8a872e8b316b
Tom Belcher <tbelcher@verizon.net>
2019-09-24 16:41:18 -0400

    deleting workfile

--- 01-webotron/ipythonsession.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-# pipenv session setup at start
-# to run this script, type the following from project folder in Powershell:
-#   pipenv run ipython -i ipythonsession.py
-
-import boto3
-session = boto3.Session('profile_name=pythonAutomation')
-s3 = session.resource('s3')
