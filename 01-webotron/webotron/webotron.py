c68ed227166db457632c3478efe2a88b603e44dc
Tom Belcher <tbelcher@verizon.net>
2019-09-30 10:48:15 -0400

    adding profile option

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -17,13 +17,22 @@ import click           # for argument processing
 
 from bucket import BucketManager
 
-SESSION = boto3.Session(profile_name='pythonAutomation')
-BUCKET_MANAGER = BucketManager(SESSION)
-
+SESSION = None
+BUCKET_MANAGER = None
 
 @click.group()
-def cli():
+@click.option('--profile', default=None,
+              help="Use a given AWS profile.")
+def cli(profile):
     """Webotron deploys websites to AWS."""
+    global SESSION, BUCKET_MANAGER  # declaring variables as global
+
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+
+    SESSION = boto3.Session(**session_cfg)
+    BUCKET_MANAGER = BucketManager(SESSION)
 
 
 @cli.command('list-buckets')   # this is a decorator controlling execution
