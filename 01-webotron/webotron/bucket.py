46d4e77143999883aeb4f9c3267a319555981c95
Tom Belcher <tbelcher@verizon.net>
2020-02-02 21:07:42 -0500

    Clean up and refactor

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -1,70 +1,41 @@
 # -*- coding: utf-8 -*-
 
-"""bucket:  Utility functions for Deploying websites with AWS.
-
-bucket provides subprograms to automate deploying static websites to AWS
-- Configure AWS S3 buckets
-  - Create them
-  - Set them up for static website hosting
-  - Deploy local files to them
-"""
-
+"""Classes for S3 Buckets."""
 
 from pathlib import Path
 import mimetypes
-from functools import reduce
 
 from botocore.exceptions import ClientError
 
-from hashlib import md5
-import util  # import util.py code
-import boto3
 
 class BucketManager:
     """Manage an S3 Bucket."""
 
-    CHUNK_SIZE = 8388608
-
-    def __init__(self, SESSION):
-        """Create a BucketManager object using passed session creds."""
-        self.s_three = SESSION.resource('s3')
-        self.transfer_config = boto3.s3.transfer.TransferConfig(
-            multipart_chunksize=self.CHUNK_SIZE,
-            multipart_threshold=self.CHUNK_SIZE
-        )
-
-        self.manifest = {}
-
-
-    def get_region_name(self, bucket):
-        """Get the bucket's region name."""
-        client = self.s_three.meta.client
-        bucket_location = client.get_bucket_location(Bucket=bucket.name)
-
-        return bucket_location["LocationConstraint"] or 'us-east-1'
-
-    def get_bucket_url(self, bucket):
-        """Generate bucket URL using Named Tuple."""
-        return "http://{}.{}".format(bucket.name,
-                                     util.get_endpoint
-                                     (self.get_region_name(bucket)).host)
+    def __init__(self, session):
+        """Create a BucketManager object."""
+        self.s3 = session.resource('s3')
 
     def all_buckets(self):
         """Get an iterator for all buckets."""
-        return self.s_three.buckets.all()
+        return self.s3.buckets.all()
 
-    def all_objects(self, bucket_name):
-        """Get an iterator for all objects."""
-        return self.s_three.Bucket(bucket_name).objects.all()
+    def all_objects(self, bucket):
+        """Get an iterator for all objects in a bucket."""
+        return self.s3.Bucket(bucket).objects.all()
 
     def init_bucket(self, bucket_name):
-        """Initiate bucket creation."""
+        """Create new bucket or return exisitng one by name."""
         s3_bucket = None
         try:
-            s3_bucket = self.s_three.create_bucket(Bucket=bucket_name)
+            s3_bucket = self.s3.create_bucket(
+                Bucket=bucket_name
+                # Bucket=bucket,
+                # CreateBucketConfiguration={'LocationConstraint': 'us-east-2'}
+            )
         except ClientError as error:
             if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-                s3_bucket = self.s_three.Bucket(bucket_name)
+                print(error)
+                s3_bucket = self.s3.Bucket(bucket_name)
             else:
                 raise error
 
@@ -72,7 +43,7 @@ class BucketManager:
 
     @staticmethod
     def set_policy(bucket):
-        """Apply web policies to bucket."""
+        """Set bucket policy to be readable by everyone."""
         policy = """
         {
             "Version":"2012-10-17",
@@ -88,12 +59,13 @@ class BucketManager:
         }
         """ % bucket.name
         policy = policy.strip()
+
         pol = bucket.Policy()
         pol.put(Policy=policy)
 
     @staticmethod
-    def set_config(bucket):
-        """Apply web config settings to bucket."""
+    def configure_website(bucket):
+        """Configure website for Error and Index documents."""
         bucket.Website().put(WebsiteConfiguration={
             'ErrorDocument': {
                 'Key': 'error.html'
@@ -103,82 +75,30 @@ class BucketManager:
             }
         })
 
-    def load_manifest(self, bucket):
-        """Load manifest for caching purposes."""
-        paginator = self.s_three.meta.client.get_paginator('list_objects_v2')
-        for page in paginator.paginate(Bucket=bucket.name):
-            for obj in page.get('Contents', []):
-                self.manifest[obj['Key']] = obj['ETag']
-
     @staticmethod
-    def hash_data(data):
-        """Generate md5 hash for data."""
-        hash = md5()
-        hash.update(data)
-
-        return hash
-
-    def gen_etag(self, path):
-        """Generate etag for file."""
-        hashes = []
-
-        with open(path, 'rb') as f:   # open file for read binary mode
-            while True:
-                data = f.read(self.CHUNK_SIZE)
-
-                if not data:
-                    break
-
-                hashes.append(self.hash_data(data))
-
-        if not hashes:
-            return
-        elif len(hashes) == 1:
-            return '"{}"'.format(hashes[0].hexdigest()) # '"{}"' prefix is
-            # due to the double quotes inside the etag value string - using
-            # format to perform string replacement at brackets
-        else:  # below, all hashes are created and recursively concatanated
-            # and then hashed again.  this is the process aws uses to get a hash
-            # for a multi-part upload
-            hash = self.hash_data(reduce(lambda x, y: x+y, (h.digest() for h in hashes)))
-            # hash of hashes returned along with number of chunks in line below
-            return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
-
-    def upload_file(self, bucket, path, key):
-        """Upload file to s3 bucket."""
-        print("upload-file with: ", bucket, path, key)
+    def upload_file(bucket, path, key):
+        """Upload object to S3 bucket."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-
-        etag = self.gen_etag(path)
-        # in line below, if hashes are equal then return, otherwise upload file
-        if self.manifest.get(key, '') == etag:
-            print("Skipping {}, etags match".format(key))
-            return
 
         return bucket.upload_file(
             path,
             key,
             ExtraArgs={
                 'ContentType': content_type
-            },
-            Config=self.transfer_config
-            )
+            })
 
     def sync(self, pathname, bucket_name):
-        """Sync contents of PATHNAME to BUCKET."""
-        bucket = self.s_three.Bucket(bucket_name)
-        self.load_manifest(bucket)
+        """Sync local folder to S3 bucket."""
+        bucket = self.s3.Bucket(bucket_name)
 
         root = Path(pathname).expanduser().resolve()
-        print('root has been set to:', root)
 
         def handle_directory(target):
-            """Construct path name."""
-            for path_1 in target.iterdir():
-                if path_1.is_dir():
-                    handle_directory(path_1)
-                if path_1.is_file():
-                    self.upload_file(bucket, str(path_1),
-                                     str(path_1.relative_to(root)))
+            for path in target.iterdir():
+                if path.is_dir():
+                    handle_directory(path)
+                if path.is_file():
+                    self.upload_file(bucket, str(path),
+                                     str(path.relative_to(root)))
 
         handle_directory(root)
--- 01-webotron/webotron/webotronlistonly.py
+++ 01-webotron/webotron/webotronlistonly.py
@@ -1,3 +1,4 @@
+"""Webotron:  list only test script."""
 import boto3
 import click
 # import sys
@@ -6,11 +7,13 @@ import click
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
+
 @click.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets."""
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 if __name__ == '__main__':
     list_buckets()
