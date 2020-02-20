66e2652709dceb9d4564fddaa455bd200c05e448
Tom Belcher <tbelcher@verizon.net>
2020-02-20 14:06:04 -0500

    syncing

--- 02-notifon/autoscale_example.py
+++ 02-notifon/autoscale_example.py
@@ -9,3 +9,4 @@ as_client.describe_auto_scaling_groups()
 as_client.describe_policies()
 as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
 as_client.execute_policy(AutoScalingGroupName='Notifon Example', PolicyName='Scale Up')
+
