4f00ad835440bf4db1e7ab8df566ac139a839ce8
Tom Belcher <tbelcher@verizon.net>
2020-02-25 19:18:44 -0500

    redoing commit

--- 02-notifon/notifier/handler.py
+++ 02-notifon/notifier/handler.py
@@ -5,6 +5,7 @@ import requests
 def post_to_slack(event, context):
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
 
+
 # following data assignment uses the glob object for **event - this allows
 # overlay of data fields using the formatting command. Note that all
 # globbed fields must exist or it will throw an error.
